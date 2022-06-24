cd686915dde4174796fbd32a3eab0f4f441bbd0d
tashironaoya <ac708708@gmail.com>
2022-06-24 22:42:37 +0900

    p52request response (controller記述)

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,26 +7,27 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response) {
-        $html = <<<EOF
-        <html>
-        <head>
-        <title>Hello/Index</title>
-        <style>
-        body { font-size:16pt, color:#999; }
-        h1 { font-size:120pt, text-align:right, color:#fafafa; }
-        </style>
-        </head>
-        <body>
-        <h3>Request</h3>
-        <pre>{$request}</pre>
-        <h3>Response</h3>
-        <pre>{$response}</pre>
-        </body>
-        </html>
-        EOF;
+    // public function index(Request $request, Response $response) {
+    //     $html = <<<EOF
+    //     <html>
+    //     <head>
+    //     <title>Hello/Index</title>
+    //     <style>
+    //     body { font-size:16pt, color:#999; }
+    //     h1 { font-size:120pt, text-align:right, color:#fafafa; }
+    //     </style>
+    //     </head>
+    //     <body>
+    //     <h3>Request</h3>
+    //     <pre>{$request}</pre>
+    //     <h3>Response</h3>
+    //     <pre>{$response}</pre>
+    //     </body>
+    //     </html>
+    //     EOF;
 
-        $response->setContent($html);
-        return $response;
-    }
+    //     $response->setContent($html);
+    //     return $response;
+    // }
+    
 }
