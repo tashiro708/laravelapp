579aff3a24ea785ce4489d7a115b0e7595431ef3
tashironaoya <ac708708@gmail.com>
2022-07-06 00:58:43 +0900

    no message

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -7,16 +7,12 @@ h1 { font-size:120pt; text-align:right; color:#fafafa; margin:-50px 0px -100px 0
 </style>
 </head>
 <body>
-<h1>Blade/Index</h1>
-@isset ($msg)
-<p>こんにちは、{{$msg}}さん。</p>
-@else
-<p>何か書いてください</p>
-@endisset
-<form method="POST" action="/hello">
-    @csrf
-    <input type="text" name="msg">
-    <input type="submit">
-</from>
+    <h1>Blade/Index</h1>
+    <p>foreachディレクティブの例</p>
+    <ol>
+    @foreach($data as $item)
+    <li>{{$item}}
+    </ol>
+    @endforeach
 </body>
 </html>
