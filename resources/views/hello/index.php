6a3ed81c0202a82c60cf2eb4d384ec91cddd5c42
tashironaoya <ac708708@gmail.com>
2022-07-06 01:46:53 +0900

    チェックアウト練習

--- resources/views/hello/index.php
+++ resources/views/hello/index.php
@@ -9,5 +9,6 @@ h1 { font-size:120pt; text-align:right; color:#fafafa; margin:-50px 0px -100px 0
 <body>
 <h1>Request</h1>
 <p>This is a sample page with php-template.</p>
+<p>a</p>
 </body>
 </html>
